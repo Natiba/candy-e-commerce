17c85ce118b68e567a3a9fffd4b10b94bb6db4ae
Nati Ba <nzbartolome@gmail.com>
2021-05-26 17:10:48 -0300

    avanzo muy poco

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,97 +1,106 @@
-import products from './db.js';
+import products from "./db.js";
 
-const container = document.getElementById('container'); //cambio promo x container!!
-const btnAlfajor = document.getElementById('alfajor');
-const btnChicle = document.getElementById('chicle');
-const btnSnack = document.getElementById('snack');
-const btnGalles = document.getElementById('galletita');
-const btnChoco = document.getElementById('chocolate');
-const btnChupetin = document.getElementById('chupetin');
+const container = document.getElementById("container"); 
+const container2 = document.getElementById("container2");
+const infoProdCont = document.getElementById("info-prod");
 
+const btnAlfajor = document.getElementById("alfajor");
+const btnChicle = document.getElementById("chicle");
+const btnSnack = document.getElementById("snack");
+const btnGalles = document.getElementById("galletita");
+const btnChoco = document.getElementById("chocolate");
+const btnChupetin = document.getElementById("chupetin");
 
-btnAlfajor.addEventListener('click', () => {
-  getProducts('alfajor');
+btnAlfajor.addEventListener("click", () => {
+  getProducts("alfajor");
 });
-btnChicle.addEventListener('click', () => {
-  getProducts('chicle');
+btnChicle.addEventListener("click", () => {
+  getProducts("chicle");
 });
-btnSnack.addEventListener('click', () => {
-  getProducts('snacks');
+btnSnack.addEventListener("click", () => {
+  getProducts("snacks");
 });
-btnGalles.addEventListener('click', () => {
-  getProducts('galletita');
+btnGalles.addEventListener("click", () => {
+  getProducts("galletita");
 });
-btnChoco.addEventListener('click', () => {
-  getProducts('chocolate');
+btnChoco.addEventListener("click", () => {
+  getProducts("chocolate");
 });
-btnChupetin.addEventListener('click', () => {
-  getProducts('chupetin');
+btnChupetin.addEventListener("click", () => {
+  getProducts("chupetin");
 });
 
 const getProducts = (categoria) => {
-
   while (container.hasChildNodes()) {
     container.removeChild(container.lastChild);
   }
 
-  let total =0;
+  let total = 0;
 
-  products.forEach(product => {
+  products.forEach((product) => {
     if (product.category === categoria) {
-      let image = document.createElement('img');
+      let image = document.createElement("img");
       image.src = product.image;
-      image.setAttribute('class', 'container-prods-img');
+      image.setAttribute("class", "container-prods-img");
       container.appendChild(image);
 
-      let h2 = document.createElement('h2');
-      h2.setAttribute('class', 'titulo');
+      let h2 = document.createElement("h2");
+      h2.setAttribute("class", "titulo");
+      h2.setAttribute("class", "info-prod-cont");
       h2.textContent = product.name;
-      h2.setAttribute('class', 'container-prods')
+      h2.setAttribute("class", "container-prods");
       container.appendChild(h2);
 
-      let p1 = document.createElement('p');
+      let p1 = document.createElement("p");
       p1.textContent = product.price;
-      p1.setAttribute('class', 'container-prods')
+      p1.setAttribute("class", "container-prods");
       container.appendChild(p1);
 
-      if (product.stock > 0) {
+      // ----- pruebo agregar div contenedeor ---
+        let divInfo = document.createElement("div");
 
-        let count = document.createElement('input'); // Crea un input
-        count.setAttribute('type', 'number'); // A ese input de asigna un atributo (type="number")
-        count.setAttribute('id', `count_${product.id}`); // A ese input de doy un id (id="product_NUMERO")
-        count.setAttribute('value', 1); // A ese input le asignamois el valor 1
-       count.setAttribute('class', 'container-prods')
+        divInfo.setAttribute("class", "info-prod-cont");
+        container.appendChild(divInfo);
+        // hasta aca
+
+      if (product.stock > 0) {
+        let count = document.createElement("input"); // Crea un input
+        count.setAttribute("type", "number"); // A ese input de asigna un atributo (type="number")
+        count.setAttribute("id", `count_${product.id}`); // A ese input de doy un id (id="product_NUMERO")
+        count.setAttribute("value", 1); // A ese input le asignamois el valor 1
+        count.setAttribute("class", "container-prods");
         container.appendChild(count); // insertamos en el container ese input
 
         let cantidad = document.getElementById(`count_${product.id}`); // Capturamos ese input para manipularlo
 
-        let btnPurchase = document.createElement('button'); // Crea un botón
-        btnPurchase.setAttribute('id', `btn_${product.id}`); // Le da una ID
-        btnPurchase.textContent = 'Agregar al carrito'; // Le da un contenido en forma de texto
-        btnPurchase.setAttribute('class', 'container-prods')
+        
+
+        let btnPurchase = document.createElement("button"); // Crea un botón
+        btnPurchase.setAttribute("id", `btn_${product.id}`); // Le da una ID
+        btnPurchase.textContent = "Agregar al carrito"; // Le da un contenido en forma de texto
+        btnPurchase.setAttribute("class", "container-prods-btn");
         container.appendChild(btnPurchase); //insertamos en el container ese botón
 
-        btnPurchase.addEventListener('click', () => { // Al botón le asignamos un evento de escucha
-          
+        btnPurchase.addEventListener("click", () => {
+          // Al botón le asignamos un evento de escucha
+
           let subtotal = product.price * cantidad.value; // Cantidad * precio
-          let getLocalStorage = localStorage.getItem('total')
+          let getLocalStorage = localStorage.getItem("total");
           console.log(getLocalStorage);
           if (getLocalStorage) {
-            total = Number(getLocalStorage) + Number(subtotal)
-            console.log(total)
+            total = Number(getLocalStorage) + Number(subtotal);
+            console.log(total);
           }
           // total += subtotal;
           console.log(total);
-          localStorage.setItem('total', total)
+          localStorage.setItem("total", total);
           /*  */
         });
-
       } else {
-        let p = document.createElement('p');
-        p.textContent = 'No hay en stock';
+        let p = document.createElement("p");
+        p.textContent = "No hay en stock";
         container.appendChild(p);
       }
     }
   });
 };
-
